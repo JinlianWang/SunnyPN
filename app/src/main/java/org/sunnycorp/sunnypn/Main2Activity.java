6d776ed096cd7b41a845aefcd69bdb3d87b210b4
Sunny Wang <wangjinlian@hotmail.com>
2017-10-08 16:07:11 -0400

    working with click_action

---
+++ app/src/main/java/org/sunnycorp/sunnypn/Main2Activity.java
@@ -0,0 +1,18 @@
+package org.sunnycorp.sunnypn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+
+}
